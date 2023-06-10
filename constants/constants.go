c455a409eff9e271bf2f2d8b56ff657199891e9e
catfish0w0 <121464779+catfish0w0@users.noreply.github.com>
2023-06-10 12:04:28 -0400

    initial commit

---
+++ constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	APP_INDEX      = "app"
+	USER_INDEX     = "user"
+	ES_URL         = "http://35.243.172.250:9200"
+	ES_USERNAME    = "gary"
+	ES_PASSWORD    = "et060202"
+	STRIPE_API_KEY = "sk_test_51MMemlKTq2qrgCa8mdacvAghuREYoc3OTvvfbZEgncDLtNnpHPPggMScbzTq4zn7oyvbmNymio7Xd29Vi8gDHppB00XA8pV6uO"
+	GCS_BUCKET     = "appstore_bucket123"
+)
